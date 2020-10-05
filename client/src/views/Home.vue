fa0a0a6c1704e4ea1104bbf5419f1627fae80aea
Dan <daniel@rpimentel.com>
2020-10-05 14:24:45 -0400

    just copying over class stuff with project files

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    <h1>Welcome to our home page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
