da24016d396c5cd5837c36bda4a3a0ef43ecc9e3
Dan <daniel@rpimentel.com>
2020-10-09 16:04:23 -0400

    navbar burger working

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-fixed-top-info" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
           <a class="navbar-item" href="https://bulma.io">
             <img src="../assets/logo.png">
           </a>
       
-          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a role="button" class="navbar-burger burger" :class="{'is-active':isActive}" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
       
-        <div id="navbarBasicExample" class="navbar-menu">
+        <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active':isActive}">
           <div class="navbar-start">
             <router-link to="/" class="navbar-item">Home</router-link> 
             <router-link to="/about"  class="navbar-item">About</router-link>
@@ -54,6 +54,11 @@
 
 <script>
   export default{
-    
+    data: ()=> ({
+      isActive:false
+    }),
+    methods:{
+
+    }
   }
 </script>
