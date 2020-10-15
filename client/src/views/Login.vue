0dc4f4f46a6c14582e2fc21da53c931b68ccbaa7
Dan <daniel@rpimentel.com>
2020-10-15 10:52:17 -0400

    login functionality for multiple users

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,7 +4,8 @@
 <form>
   <div class="field">
   <p class="control has-icons-left has-icons-right">
-    <input class="input" type="email" placeholder="Email">
+    <input class="input" type="text" v-model="username">
+    {{username}}
     <span class="icon is-small is-left">
       <i class="fas fa-envelope"></i>
     </span>
@@ -36,13 +37,12 @@
 <script>
 import session from "@/models/session";
 export default {
+  data: ()=>({
+    username: ""
+  }),
   methods: {
     login(){
-      session.user={
-            name:'Maria De La Cruz',
-            handle: 'maria.pimentel.908',
-            profile: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/120970608_3675989932451570_8765047977384829050_n.jpg?_nc_cat=110&_nc_sid=09cbfe&_nc_ohc=JnTC5Xk8ZqsAX8xq0U6&_nc_ht=scontent-lga3-1.xx&oh=0f17e668998362917926f9d1434a69cc&oe=5FA6E0CA'
-      }
+      session.user=session.users.find(x => x.handle == this.username)
       this.$router.push('feed');
     },
   }
