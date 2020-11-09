e01e4fb008f99da72788bd69217540a05cfca768
Dan <daniel@rpimentel.com>
2020-11-09 13:21:51 -0500

    creating server and starting user controller

---
+++ server/controllers/users.js
@@ -0,0 +1,10 @@
+/*
+    This gets all of the gets things from the users table 
+*/
+
+const express = require('express');
+const users = require('../models/users');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+/*
+    starting portion of the server breaks of into smaller controllers 
+*/
+
+const express = require('express');
+require('dotenv').config();
+
+const users = require('./controllers/users');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(function(req,res,next){
+    const arr = (req.headers.authorization ||"").split(" ");
+    if ( arr.length > 1 && arr[1] != null ){
+        req.userId = +arr[1];
+    }
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hi this is me working on my fitness project');
+})
+
+app.use('/users',users);
+
+app.use((err,req,res,next)=>{
+    console.log(err);
+    res.status( err.status || 500 ).send( { message: err.message } )
+})
+
+app.listen(port,()=>{
+    console.log(`Fitness app listening at http://localhost:${port}`);
+})
