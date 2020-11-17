c2d9aec4257396a075ab93c12aa0cedc4d3ae8a2
Dan <daniel@rpimentel.com>
2020-11-16 21:48:21 -0500

    comments model search and update

--- server/models/comments.js
+++ server/models/comments.js
@@ -21,13 +21,26 @@ async function getAllUsersComments(id){
     FROM ${PREFIX}Comments AS C WHERE C.Owner_id = ?`;
     return await mysql.query(sql,[id]);
 }
+async function get(id){
+    const sql = `SELECT *
+    FROM ${PREFIX}Comments WHERE id=?`
+    const rows = await mysql.query(sql,[id]);
+    if(!rows.length)throw{ status:404, message:"Sorry, there is no such comment" };
+    return rows[0];
+}
 async function add(Text, Workout_id, Owner_id){
     const sql = `INSERT INTO ${PREFIX}Comments (created_at, Text, Workout_id, Owner_id) VALUES (?)`
     const params =[[new Date(),Text, Workout_id, Owner_id]];
     return await mysql.query(sql,params);
 } 
+async function update(id, Text, Workout_id, Owner_id){
+    const sql = `UPDATE ${PREFIX}Comments SET ? WHERE id = ? ;`
+    const params =[[Text, Workout_id, Owner_id]];
+    return await mysql.query(sql,[params,id]);
+} 
 async function remove(id){
     const sql = `DELETE FROM ${PREFIX}Comments WHERE id = ?`;
     return await mysql.query(sql,[id]);
 }
+const search = async q => mysql.query(`SELECT id,Text,Post_id FROM ${PREFIX}Comments WHERE Text LIKE ?;`, [`%${q}%`]);
 module.exports = { getAll, getWorkoutComments, getAllUsersComments, add, remove };
